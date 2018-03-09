a9bb67d9662ca46e9d1a709cbb515d54b17c0775
Палёный Алексей Эдуардович <palenyj.ae@edu.spbstu.ru>
2018-03-09 12:48:03 +0300

    Added function for showing error by using file and offset

---
+++ CTHRY1/Common.h
@@ -0,0 +1,12 @@
+#pragma once
+/* This header contains only auxiliary common functions */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+
+
+void showError(FILE *inFile, unsigned int FilePos);
+
+#endif // !COMMON_H
